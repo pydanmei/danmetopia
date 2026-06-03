7ad6801741030f1098389f8c00cd98a1cc20553d
pydanmei <pydanmeii@gmail.com>
2026-06-03 13:00:47 +0200

    Create state.js

---
+++ js/state.js
@@ -0,0 +1,12 @@
+export const state = {
+  currentUser: null,
+  stories: [],
+  allGroups: [],
+  userFollows: {},
+  readingHistory: [],
+  bookmarks: [],
+  currentTab: "all",
+  selectedGenre: "",
+  searchKeyword: "",
+  sortBy: "likes"
+};
